e63637d2e9b4581de3cd3228a7e4369aee829e43
highlander <highlander9200@gmail.com>
2023-12-14 13:54:45 -0300

    Start a Spring Boot project with its dependencies, create a Client entity, configure the in-memory database and create a test profile for the in-memory database use

---
+++ src/test/java/com/highlander/DesafioCrudDeClientes/DesafioCrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highlander.DesafioCrudDeClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
